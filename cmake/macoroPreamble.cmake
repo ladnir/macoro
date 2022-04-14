e70fec7db7715af945921126e293c19200ba854c
Peter Rindal <peterrindal@gmail.com>
2022-04-14 00:41:56 -0700

    ci

--- cmake/macoroPreamble.cmake
+++ cmake/macoroPreamble.cmake
@@ -1,7 +1,11 @@
 
+if(NOT DEFINED CMAKE_BUILD_TYPE)
+    set(CMAKE_BUILD_TYPE "Release")
+endif()
+
 if(MSVC)
     if("${CMAKE_BUILD_TYPE}" STREQUAL "RelWithDebInfo")
-        set(MACORO_CONFIG "x64-${CMAKE_BUILD_TYPE}")
+        set(MACORO_CONFIG "x64-Release")
     else()
         set(MACORO_CONFIG "x64-${CMAKE_BUILD_TYPE}")
     endif()

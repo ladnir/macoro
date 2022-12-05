179f1a5b1ee999b155211ec3eef79af33b8e7251
Peter Rindal <peterrindal@gmail.com>
2022-12-05 08:19:21 -0800

    improved find_package options

--- cmake/macoroBuildOptions.cmake
+++ cmake/macoroBuildOptions.cmake
@@ -6,9 +6,10 @@ include("${CMAKE_CURRENT_LIST_DIR}/macoroPreamble.cmake")
 set(MACORO_BUILD ON)
 
 
-
+option(MACORO_FETCH_AUTO "" OFF)
 option(MACORO_PIC "build with -FPIC on unix" OFF)
 option(MACORO_INSTALL_THIRDPARTY "dont install third party" ON)
+option(MACORO_ASAN "build with asan" OFF)
 
 if(NOT DEFINED MACORO_THIRDPARTY_CLONE_DIR)
     set(MACORO_THIRDPARTY_CLONE_DIR "${CMAKE_CURRENT_LIST_DIR}/../out/")
@@ -46,7 +47,7 @@ endif()
 
 message("\nOptions\n---------------------------")
 
-option(MACORO_FETCH_AUTO "" OFF)
+message("CMAKE_BUILD_TYPE       = ${CMAKE_BUILD_TYPE}\t  ~ build type")
 message("MACORO_FETCH_AUTO      = ${MACORO_FETCH_AUTO}\t  ~ automatically fetch dependencies as needed")
 message("MACORO_FETCH_OPTIONAL  = ${MACORO_FETCH_OPTIONAL}  \t  ~ fetch optional-lite")
 message("MACORO_FETCH_VARIANT   = ${MACORO_FETCH_VARIANT}  \t  ~ fetch variant-lite")
